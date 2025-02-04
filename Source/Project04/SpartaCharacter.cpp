d14674a968f7da3208e6d777aba3162ab3369e3c
dlrmsgud <wkdlantm@naver.com>
2025-02-05 06:01:29 +0900

    Add files via upload

---
+++ Source/Project04/SpartaCharacter.cpp
@@ -0,0 +1,130 @@
+#include "SpartaCharacter.h"
+#include "SpartaPlayerController.h"
+#include "EnhancedInputComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+ASpartaCharacter::ASpartaCharacter()
+{
+ 	PrimaryActorTick.bCanEverTick = false;
+
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SorinArm"));
+	SpringArmComp->SetupAttachment(RootComponent);
+	SpringArmComp->TargetArmLength = 300.0f;
+	SpringArmComp->bUsePawnControlRotation = true;
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("Camra"));
+	CameraComp->SetupAttachment(SpringArmComp, USpringArmComponent::SocketName);
+	CameraComp->bUsePawnControlRotation = false;
+}
+
+void ASpartaCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASpartaCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ASpartaCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EnhancedInput = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		if (ASpartaPlayerController* PlayerController = Cast<ASpartaPlayerController>(GetController()))
+		{
+			if (PlayerController->MoveAction)
+			{
+				EnhancedInput->BindAction(
+					PlayerController->MoveAction,
+					ETriggerEvent::Triggered,
+					this,
+					&ASpartaCharacter::Move
+				);
+			}
+
+			if (PlayerController->JumpAction)
+			{
+				EnhancedInput->BindAction(
+					PlayerController->JumpAction,
+					ETriggerEvent::Triggered,
+					this,
+					&ASpartaCharacter::StartJump
+				);
+
+
+				EnhancedInput->BindAction(
+					PlayerController->JumpAction,
+					ETriggerEvent::Completed,
+					this,
+					&ASpartaCharacter::StopJump
+				);
+
+
+			}
+			if (PlayerController->LookAction)
+			{
+				EnhancedInput->BindAction(
+					PlayerController->LookAction,
+					ETriggerEvent::Triggered,
+					this,
+					&ASpartaCharacter::Look
+				);
+			}
+
+			if (PlayerController->SprintAction)
+			{
+				EnhancedInput->BindAction(
+					PlayerController->SprintAction,
+					ETriggerEvent::Triggered,
+					this,
+					&ASpartaCharacter::StartSprint
+				);
+
+
+				EnhancedInput->BindAction(
+					PlayerController->SprintAction,
+					ETriggerEvent::Completed,
+					this,
+					&ASpartaCharacter::StopSprint
+				);
+			}
+		}
+	}
+}
+
+void ASpartaCharacter::Move(const FInputActionValue& value)
+{
+	if (!Controller) return;
+
+	const FVector2D MoveInput = value.Get<FVector2D>();
+
+	if (!FMath::IsNearlyZero(MoveInput.X))
+	{
+		AddMovementInput(GetActorForwardVector(), MoveInput.X);
+	}
+	if (!FMath::IsNearlyZero(MoveInput.Y))
+	{
+		AddMovementInput(GetActorRightVector(), MoveInput.Y);
+	}
+}
+void ASpartaCharacter::StartJump(const FInputActionValue& value)
+{
+}
+void ASpartaCharacter::StopJump(const FInputActionValue& value)
+{
+}
+void ASpartaCharacter::Look(const FInputActionValue& value)
+{
+}
+void ASpartaCharacter::StartSprint(const FInputActionValue& value)
+{
+}
+void ASpartaCharacter::StopSprint(const FInputActionValue& value)
+{
+}
